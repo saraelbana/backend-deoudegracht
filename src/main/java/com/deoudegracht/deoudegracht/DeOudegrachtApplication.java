6aca14ce0ffb80537b2c46f3fca4536b97c75741
saraelbana <matika20@gmail.com>
2024-02-24 01:45:59 +0100

    Initial commit

---
+++ src/main/java/com/deoudegracht/deoudegracht/DeOudegrachtApplication.java
@@ -0,0 +1,13 @@
+package com.deoudegracht.deoudegracht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeOudegrachtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeOudegrachtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deoudegracht/deoudegracht/DeOudegrachtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deoudegracht.deoudegracht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeOudegrachtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
