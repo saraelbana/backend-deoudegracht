575035988b1f72d136d617f0f6113e9c65ce5daf
saraelbana <matika20@gmail.com>
2024-02-24 12:40:23 +0100

    starting implemnting basic properties in guest class

--- src/main/java/com/deoudegracht/deoudegracht/models/Guest.java
+++ src/main/java/com/deoudegracht/deoudegracht/models/Guest.java
@@ -1,4 +1,14 @@
 package com.deoudegracht.deoudegracht.models;
 
 public class Guest {
+    private Long ID;
+    public Guest() {
+    }
+    public Long getID() {
+        return ID;
+    }
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
 }
