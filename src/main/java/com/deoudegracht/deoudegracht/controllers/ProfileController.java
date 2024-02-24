a3731248b1f52ceea75aa6a1b19ff6233cfb4c34
saraelbana <matika20@gmail.com>
2024-02-24 02:45:50 +0100

    still on building the basic files structures and a pair of controller files and Requests and Respondes DTO

---
+++ src/main/java/com/deoudegracht/deoudegracht/controllers/ProfileController.java
@@ -0,0 +1,7 @@
+package com.deoudegracht.deoudegracht.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ProfileController {
+}
---
+++ src/main/java/com/deoudegracht/deoudegracht/dtos/ProfileRequestDTO.java
@@ -0,0 +1,4 @@
+package com.deoudegracht.deoudegracht.dtos;
+
+public class ProfileRequestDTO {
+}
