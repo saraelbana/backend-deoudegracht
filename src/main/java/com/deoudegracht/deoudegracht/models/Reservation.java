1be24d97d24cc7e0e2be2d9517003e7c41ac8b98
saraelbana <matika20@gmail.com>
2024-02-24 12:44:57 +0100

    starting implemnetation of Reservation class

--- src/main/java/com/deoudegracht/deoudegracht/models/Reservation.java
+++ src/main/java/com/deoudegracht/deoudegracht/models/Reservation.java
@@ -1,4 +1,45 @@
 package com.deoudegracht.deoudegracht.models;
 
+import java.time.LocalDate;
+
 public class Reservation {
+    private Long ID;
+    private LocalDate date;
+    private Integer numberOfPersons;
+    private Guest guest;
+
+    public Reservation() {
+    }
+
+    public Long getID() {
+        return ID;
+    }
+
+    public void setID(Long ID) {
+        this.ID = ID;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Integer getNumberOfPersons() {
+        return numberOfPersons;
+    }
+
+    public void setNumberOfPersons(Integer numberOfPersons) {
+        this.numberOfPersons = numberOfPersons;
+    }
+
+    public Guest getGuest() {
+        return guest;
+    }
+
+    public void setGuest(Guest guest) {
+        this.guest = guest;
+    }
 }
