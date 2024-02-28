71d4c16eb16e852e651ab9c6ca8b699640240b62
Sara <sara@Mostafas-MacBook-Pro.local>
2024-02-28 18:43:13 +0100

    creating the repo class by extending the JPAREppository

--- src/main/java/com/deoudegracht/deoudegracht/repositories/EmployeeRepository.java
+++ src/main/java/com/deoudegracht/deoudegracht/repositories/EmployeeRepository.java
@@ -1,2 +1,7 @@
-package com.deoudegracht.deoudegracht.repositories;public interface EmployeeRepository {
+package com.deoudegracht.deoudegracht.repositories;
+
+import com.deoudegracht.deoudegracht.models.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
