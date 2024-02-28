5b3c2a14e11060f97977e7c536f0b4ff5691c95d
Sara <sara@Mostafas-MacBook-Pro.local>
2024-02-28 18:41:36 +0100

    creating the employee service layer and injecting the repository in it

--- src/main/java/com/deoudegracht/deoudegracht/services/EmployeeService.java
+++ src/main/java/com/deoudegracht/deoudegracht/services/EmployeeService.java
@@ -1,2 +1,25 @@
-package com.deoudegracht.deoudegracht.services;public class EmployeeService {
+package com.deoudegracht.deoudegracht.services;
+
+import com.deoudegracht.deoudegracht.models.Employee;
+import com.deoudegracht.deoudegracht.repositories.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+    public void getEmployees() {
+    }
+    public void getEmployeeById(Long id) {
+    }
+    public void createEmployee(Employee employee) {
+        employeeRepository.save(employee);
+    }
+    public void updateEmployee(Long id) {
+    }
+    public void deleteEmployee(Long id) {
+    }
 }
