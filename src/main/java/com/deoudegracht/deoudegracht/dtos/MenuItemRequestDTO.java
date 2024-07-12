7609586fea1d6c49412e09a143197fa495c8131d
sara <matika20@gmail.com>
2024-07-12 22:35:42 +0200

    creating the responseDto

--- src/main/java/com/deoudegracht/deoudegracht/dtos/MenuItemRequestDTO.java
+++ src/main/java/com/deoudegracht/deoudegracht/dtos/MenuItemRequestDTO.java
@@ -1,4 +1,50 @@
 package com.deoudegracht.deoudegracht.dtos;
 
 public class MenuItemRequestDTO {
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+
+    public MenuItemRequestDTO() {
+    }
+
+    public MenuItemRequestDTO(String name, String description, double price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
