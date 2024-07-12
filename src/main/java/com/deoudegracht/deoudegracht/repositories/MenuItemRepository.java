486ae4e5ec3fb9d0c4733cfa239ea348cc6b6dfe
sara <matika20@gmail.com>
2024-07-12 22:37:34 +0200

    creating the repo interface

--- src/main/java/com/deoudegracht/deoudegracht/repositories/MenuItemRepository.java
+++ src/main/java/com/deoudegracht/deoudegracht/repositories/MenuItemRepository.java
@@ -1,2 +1,8 @@
-package com.deoudegracht.deoudegracht.repositories;public interface MenuItemRepository {
+package com.deoudegracht.deoudegracht.repositories;
+
+import com.deoudegracht.deoudegracht.models.Employee;
+import com.deoudegracht.deoudegracht.models.MenuItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
 }
