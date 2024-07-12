3baec85c4b496b5f5d2ea183289f92a715b44fd7
sara <matika20@gmail.com>
2024-07-12 22:39:52 +0200

    creating menu item service class

--- src/main/java/com/deoudegracht/deoudegracht/services/MenuItemService.java
+++ src/main/java/com/deoudegracht/deoudegracht/services/MenuItemService.java
@@ -1,2 +1,13 @@
-package com.deoudegracht.deoudegracht.services;public class MenuItemService {
+package com.deoudegracht.deoudegracht.services;
+
+import com.deoudegracht.deoudegracht.repositories.MenuItemRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuItemService {
+    private final MenuItemRepository menuItemRepository;
+
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
 }
