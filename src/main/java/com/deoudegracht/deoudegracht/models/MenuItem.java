a391d003f008585af86efdc213e3fcbdedf54716
sara <matika20@gmail.com>
2024-07-12 21:59:27 +0200

    creating basic parameters for a MenuItem

--- src/main/java/com/deoudegracht/deoudegracht/models/MenuItem.java
+++ src/main/java/com/deoudegracht/deoudegracht/models/MenuItem.java
@@ -1,4 +1,34 @@
 package com.deoudegracht.deoudegracht.models;
 
 public class MenuItem {
+    private String name;
+    private String description;
+    private double price;
+    private String category;
+
+    public MenuItem(String name, String description, double price, String category) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.category = category;
+    }
+
+    public MenuItem() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
 }
