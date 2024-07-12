a032e93b2ff0231c8ffec1a7f7895f180b2d2a7f
sara <matika20@gmail.com>
2024-07-12 22:08:27 +0200

    creating the basic parameters for menu class and linking it to the DB

--- src/main/java/com/deoudegracht/deoudegracht/models/Menu.java
+++ src/main/java/com/deoudegracht/deoudegracht/models/Menu.java
@@ -1,4 +1,43 @@
 package com.deoudegracht.deoudegracht.models;
 
+import jakarta.persistence.*;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "menu")
 public class Menu {
+    private String name;
+    private String description;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    public Menu(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public Menu() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
