8fc88acc61d2182e6edf08b6109f280a519ffaca
sara <matika20@gmail.com>
2024-07-12 22:48:17 +0200

    starting creating menu item controller

--- src/main/java/com/deoudegracht/deoudegracht/controllers/MenuItemController.java
+++ src/main/java/com/deoudegracht/deoudegracht/controllers/MenuItemController.java
@@ -1,7 +1,18 @@
 package com.deoudegracht.deoudegracht.controllers;
 
+import com.deoudegracht.deoudegracht.services.EmployeeService;
+import com.deoudegracht.deoudegracht.services.MenuItemService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/menu-items")
 public class MenuItemController {
+    private final MenuItemService menuItemService;
+
+    public MenuItemController(MenuItemService menuItemService) {
+        this.menuItemService = menuItemService;
+    }
 }
