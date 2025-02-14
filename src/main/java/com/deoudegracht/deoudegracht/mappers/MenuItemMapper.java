afb4077adf8307c9d4624e0972d0b52c5fdff540
Sara Elbana <150337030+saraelbana@users.noreply.github.com>
2025-02-14 08:34:44 +0100

    wokring on the mapper

--- src/main/java/com/deoudegracht/deoudegracht/mappers/MenuItemMapper.java
+++ src/main/java/com/deoudegracht/deoudegracht/mappers/MenuItemMapper.java
@@ -3,16 +3,38 @@ package com.deoudegracht.deoudegracht.mappers;
 import com.deoudegracht.deoudegracht.dtos.MenuItemRequestDTO;
 import com.deoudegracht.deoudegracht.dtos.MenuItemResponseDTO;
 import com.deoudegracht.deoudegracht.models.MenuItem;
+import com.deoudegracht.deoudegracht.services.FileStorageService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
+@Component
 public class MenuItemMapper {
-    static public MenuItemResponseDTO mapMenuItemToMenuItemResponseDTO(MenuItem menuItem) {
-        return new MenuItemResponseDTO(menuItem.getId(), menuItem.getName(), menuItem.getDescription(), menuItem.getPrice(), menuItem.getCategory(), menuItem.getRecipeId());
+    private final FileStorageService fileStorageService;
+
+    public MenuItemMapper(FileStorageService fileStorageService) {
+        this.fileStorageService = fileStorageService;
     }
-    static public MenuItem mapMenuItemRequestDTOToMenuItem(MenuItemRequestDTO menuItemRequestDTO){
-        return new MenuItem(menuItemRequestDTO.getName(), menuItemRequestDTO.getDescription(), menuItemRequestDTO.getPrice(), menuItemRequestDTO.getCategory(), menuItemRequestDTO.getRecipeId());
+
+    public MenuItemResponseDTO mapMenuItemToMenuItemResponseDTO(MenuItem menuItem) {
+        return new MenuItemResponseDTO(menuItem.getId(), menuItem.getName(), menuItem.getDescription(), 
+            menuItem.getPrice(), menuItem.getCategory(), menuItem.getRecipeId(), menuItem.getImagePath());
     }
-    static public MenuItem mapMenuItemRequestDTOToMenuItem(MenuItemRequestDTO menuItemRequestDTO, long id){
-        MenuItem menuItem =   new MenuItem(menuItemRequestDTO.getName(), menuItemRequestDTO.getDescription(), menuItemRequestDTO.getPrice(), menuItemRequestDTO.getCategory(), menuItemRequestDTO.getRecipeId());
+
+    public MenuItem mapMenuItemRequestDTOToMenuItem(MenuItemRequestDTO menuItemRequestDTO) throws IOException {
+        MultipartFile image = menuItemRequestDTO.getImage();
+        String imagePath = null;
+        if (image != null && !image.isEmpty()) {
+            imagePath = fileStorageService.storeFile(image);
+        }
+        return new MenuItem(menuItemRequestDTO.getName(), menuItemRequestDTO.getDescription(), 
+            menuItemRequestDTO.getPrice(), menuItemRequestDTO.getCategory(), 
+            menuItemRequestDTO.getRecipeId(), imagePath);
+    }
+
+    public MenuItem mapMenuItemRequestDTOToMenuItem(MenuItemRequestDTO menuItemRequestDTO, long id) throws IOException {
+        MenuItem menuItem = mapMenuItemRequestDTOToMenuItem(menuItemRequestDTO);
         menuItem.setId(id);
         return menuItem;
     }
